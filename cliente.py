b07284d4495077518da77e53ac49dd569df5a868
avelinoandre <andreavelino002@gmail.com>
2026-03-31 22:29:09 -0300

    fazendo gobackn

--- cliente.py
+++ cliente.py
@@ -1,34 +1,93 @@
-import socket
+import socket, os
+
+BLUE = "\033[34m"
+RESET = "\033[0m"
+
+def receber(client):
+    return client.recv(1024).decode()
+
+def enviar(mensagem, client):
+    client.send(mensagem.encode())
+
+def limparTerminal():
+    os.system("cls" if os.name == "nt" else "clear")
+
+def cabecalho():
+    cliente = r"""
+ ██████╗██╗     ██╗███████╗███╗   ██╗████████╗███████╗
+██╔════╝██║     ██║██╔════╝████╗  ██║╚══██╔══╝██╔════╝
+██║     ██║     ██║█████╗  ██╔██╗ ██║   ██║   █████╗  
+██║     ██║     ██║██╔══╝  ██║╚██╗██║   ██║   ██╔══╝  
+╚██████╗███████╗██║███████╗██║ ╚████║   ██║   ███████╗
+ ╚═════╝╚══════╝╚═╝╚══════╝╚═╝  ╚═══╝   ╚═╝   ╚══════╝
+"""
+    print(BLUE + cliente + RESET)
 
 def start_client():
+    limparTerminal()
+    cabecalho()
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     
     try:
         client.connect(('localhost', 8080))
 
-        #Recebe um print de server pedindo o tamanho máximo da string a ser enviada
-        informacao_inicial = client.recv(1024).decode()
-        print(informacao_inicial)
+        while True:
+            operacao = receber(client)
+            print(operacao, end="")
 
-        tamanho_mensagem = int(input(""))
-        
+            operacao_escolhida = input("-> ")
+            enviar(operacao_escolhida, client)
 
-        print(f"Informando tamanho ({tamanho_mensagem}) ao servidor.")
-        client.send(str(tamanho_mensagem).encode())
+            validacao = receber(client)
+            if validacao == "True":
+                break
+            else:
+                print(validacao, end="")
+                continue
 
-        #Recebe o retorno do servidor, caso volte "OK", o tamanho é válido
-        resposta = client.recv(1024).decode()
+        tamanho_str = receber(client)
+        print(tamanho_str, end="")
 
-        if resposta == "OK":
-            print("Servidor autorizou!\n")
-            mensagem = input(f"Informe a string que você deseja enviar (até {tamanho_mensagem}):\n")
-            #Envia a string em blocos de 4
-            for i in range(0, len(mensagem), 4):
-                fatia = mensagem[i:i+4]
-                client.send(fatia.encode())
-            print("Enviado")
+        tamanho_mensagem = input("-> ")
+        print(f"Informando tamanho ({tamanho_mensagem}) ao servidor...")
+        enviar(tamanho_mensagem, client)
+
+        aviso_janela = receber(client)
+        print(aviso_janela, end="")
+
+        resposta = receber(client)
+        print(resposta, end="")
+
+        if "Tamanho aceito" in resposta:
+            
+            while True:
+                mensagem = input(f"\nInforme a string que você deseja enviar:\n-> ")
+                
+                print("\nIniciando envio dos pacotes...")
+                deu_erro_servidor = False
+                
+                #enviando a string em blocos de 4
+                for i in range(0, len(mensagem), 4):
+                    fatia = mensagem[i:i+4]
+                    enviar(fatia, client)
+                    
+                    confirmacao = receber(client)
+                    print(f"Pacote enviado: [{fatia}] | Resposta: {confirmacao}")
+                    
+                    #Recebe a validação do server
+                    if "ERRO" in confirmacao:
+                        print(f"\n[CLIENTE] O Servidor não aceitou: {confirmacao}")
+                        deu_erro_servidor = True
+                        break
+                
+                if deu_erro_servidor:
+                    continue # Volta pro "Informe a string..."
+                else:
+                    print("\n[CLIENTE] Envio concluído com sucesso!")
+                    input("\nPressione ENTER para encerrar a conexão com o servidor...")
+                    break
         else:
-            print(f"Servidor recusou: {resposta}")
+            print("[CLIENTE] Conexão encerrada pelo servidor (tamanho recusado).")
 
     except Exception as e:
         print(f"Erro: {e}")
--- server.py
+++ server.py
@@ -1,45 +1,128 @@
-import socket
+import socket, os
+
+YELLOW = "\033[33m"
+RESET = "\033[0m"
+
+def enviar(mensagem,conn):
+    conn.send(mensagem.encode())
+
+def receber(conn):
+    return conn.recv(1024).decode()
+
+def limpar_tela():
+    os.system("cls" if os.name == "nt" else "clear")
+
+def cabecalho():
+    servidor = r"""
+███████╗███████╗██████╗ ██╗   ██╗██╗██████╗  ██████╗ ██████╗ 
+██╔════╝██╔════╝██╔══██╗██║   ██║██║██╔══██╗██╔═══██╗██╔══██╗
+███████╗█████╗  ██████╔╝██║   ██║██║██║  ██║██║   ██║██████╔╝
+╚════██║██╔══╝  ██╔══██╗╚██╗ ██╔╝██║██║  ██║██║   ██║██╔══██╗
+███████║███████╗██║  ██║ ╚████╔╝ ██║██████╔╝╚██████╔╝██║  ██║
+╚══════╝╚══════╝╚═╝  ╚═╝  ╚═══╝  ╚═╝╚═════╝  ╚═════╝ ╚═╝  ╚═╝
+"""
+    print(YELLOW + servidor + RESET)
+
+def print_asc():
+    limpar_tela()
+    cabecalho()
 
 def start_server():
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     server.bind(('localhost', 8080))
     server.listen(1)
-    print("Servidor Aguardando Conexão")
+    print_asc()
 
     conn, addr = server.accept()
     try:
-        mensagem_incial = "Qual o tamanho máximo de string que você deseja enviar? (Mínimo é 30.)\n"
-        conn.send(mensagem_incial.encode())
-
-        #Recebe o tamanho máximo da string
-        resposta = conn.recv(1024).decode()
-        if not resposta: return
-
-        tamanho_mensagem = int(resposta)
+        while True:
+            modo_operacao = "[SERVIDOR]Escolha o método da operação\n[1]Go-backn\n[2]Repetição Seletiva\n"
+            enviar(modo_operacao,conn)
+            operacao_escolhida = receber(conn)
+            if operacao_escolhida == "1":
+                operacao = 1
+                enviar("True",conn)
+                print_asc()
+                print("[CLIENTE]Operação escolhida foi [1]Go-backn\n")
+                break
+            elif operacao_escolhida == "2":
+                operacao = 2
+                enviar("True",conn)
+                print_asc()
+                print("[CLIENTE]Operação escolhida foi [2]Repetição Seletiva\n")
+                break
+            else:
+                enviar("[SERVIDOR]Erro! Opção inválida! Repetindo operação...\n",conn)
+                print_asc()
+                print("[SERVIDOR]Erro! Opção inválida! Aguardando nova resposta...")
+                continue
         
-        print(f"Cliente quer enviar uma string de tamanho {tamanho_mensagem}.")
+        enviar("[SERVIDOR]Qual o tamanho máximo de string que você deseja enviar? (Mínimo é 30.)\n",conn)
+        tamanho_mensagem = int(receber(conn))
+        
+        print(f"[SERVIDOR]Cliente quer enviar uma string de tamanho {tamanho_mensagem}.\n")
+        enviar("[SERVIDOR]Janela atual: 5 pacotes.\n",conn)
 
-        #Verifica se o tamanho é maior ou igual a 30, se sim, continua
         if tamanho_mensagem < 30:
-            mensagem_erro = f"NEGADO: Tamanho {tamanho_mensagem} é menor que o mínimo de 30."
-            conn.send(mensagem_erro.encode())
-            print("Conexão recusada por tamanho insuficiente.")
-        
+            enviar(f"[SERVIDOR]NEGADO: Tamanho {tamanho_mensagem} é menor que o mínimo de 30.", conn)
+            print("[SERVIDOR]Conexão recusada por tamanho insuficiente.")
         else:
-            conn.send("OK".encode())
-            print("Tamanho validado. Aguardando recebimento da string")
+            conn.send("[SERVIDOR]Tamanho aceito! Envie a string.\n".encode())
+            print("[SERVIDOR]Tamanho validado. Aguardando recebimento da string.\n")
 
-        #Recebe a string em blocos de 4
-        string_final = ""
-        while len(string_final) < tamanho_mensagem:
-            bloco = conn.recv(4).decode()
-            if not bloco: break
-            string_final += bloco
-            print(f"Recebido bloco: [{bloco}]")
+            if operacao in (1, 2):
+                nome_op = "Go-Back-N" if operacao == 1 else "Repetição Seletiva"
+                print(f"[SERVIDOR]Modo {nome_op} iniciado.\n")
 
-        print(f"\nSucesso! String completa: {string_final}")
+                while True:
+                    string_final = ""
+                    janela = 1
+                    janela_max = (tamanho_mensagem + 3) // 4
+                    estourou_limite = False
 
+                    while True:
+                            pacote = conn.recv(4).decode()
+                            
+                            if not pacote: break
+
+                            # valida se a quantidade de pacotes foi ultrapassada
+                            if janela > janela_max:
+                                # DISPARA O ERRO PARA O CLIENTE
+                                enviar("[SERVIDOR]ERRO: Limite excedido! Reenvie a string.", conn)
+                                print(f"\n[SERVIDOR]ERRO: Cliente tentou enviar pacote {janela}, ultrapassando o limite de {janela_max} pacotes!")
+                                estourou_limite = True
+                                
+                                try:
+                                    while conn.recv(1024): pass
+                                except socket.timeout:
+                                    pass
+                                conn.settimeout(None)
+                                break
+
+                            # Confirmação normal
+                            confirmacao = f"[SERVIDOR]ACK {janela}" if operacao == 1 else "certo"
+                            enviar(confirmacao, conn)
+                            string_final += pacote
+                            
+                            if operacao == 1:
+                                print(f"[SERVIDOR]Recebido pacote {janela}: [{pacote}]")
+                            else:
+                                print(f"[SERVIDOR]Recebido pacote: [{pacote}]")
+                                
+                            janela += 1
+
+                    if estourou_limite:
+                        print("[SERVIDOR] Exigindo reenvio. Aguardando nova string...\n")
+                        continue 
+
+                    if not estourou_limite and string_final != "":
+                        print("\n[SERVIDOR]Sucesso! String completa recebida:")
+                        print(string_final)
+                        break
+
+    except Exception as e:
+        print(f"\n[SERVIDOR] Erro ou conexão encerrada: {e}")
     finally:
         conn.close()
         server.close()
